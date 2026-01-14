1a8e6b0d28c9e35e0cb761994d9fa5d7c4681a6d
dr0drigues <dimitri.rodrigues.oliveira@gmail.com>
2026-01-14 18:08:36 +0100

    fix: fix some issues with aliases

--- functions/git_root.zsh
+++ functions/git_root.zsh
@@ -1,9 +1,11 @@
 # Se déplacer à la racine du dépôt Git actuel
-gr() {
+# Note: unalias au cas ou un alias 'gr' existe deja (oh-my-zsh, etc.)
+unalias gr 2>/dev/null
+function gr() {
   local root
   root=$(git rev-parse --show-toplevel 2>/dev/null)
   if [ -z "$root" ]; then
-    echo "Pas dans un dépôt Git."
+    echo "Pas dans un depot Git."
   else
     cd "$root"
   fi
